a48b6fcf005fea02e6f47728e8a2d35afe050800
Thomas Fox <tfo@ith-icoserve.com>
2019-11-13 11:50:32 +0100

    IEspeed Release.

---
+++ README.txt
@@ -0,0 +1,10 @@
+IEspeed is an ActiveX wrapper around CefSharp which speeds up web pages running in IE.
+
+In order to install simply run the install.cmd script. 
+This will register the IESpeedLibrary.dll in the registry.
+
+Compiled for x86.
+
+The frameworks used are:
+CefSharp: https://github.com/cefsharp/CefSharp
+Cef: https://bitbucket.org/chromiumembedded/cef/src/master/
---
+++ install.cmd
@@ -0,0 +1,54 @@
+@echo off
+echo -------------------------------------------------------
+echo  Installing IESpeed ActiveX control
+echo -------------------------------------------------------
+echo.
+set STAGEDIR=%~dp0
+set SOURCEDIR=%STAGEDIR%\Release
+set LOCALDIR=%PROGRAMFILES(x86)%\IESpeed
+set DOTNETPATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\
+
+echo SOURCE DIR: %SOURCEDIR%
+echo STAGE DIR: %STAGEDIR%
+echo LOCAL DIR: %LOCALDIR%
+echo.
+
+rem check Admin Permissions
+net session >nul 2>&1
+if errorLevel 1  (
+    echo *** ERROR: Please run this script as Administrator
+	echo.
+	pause
+	exit
+)
+
+robocopy /R:0 /W:0 /NFL /NDL /MIR "%SOURCEDIR%" "%LOCALDIR%" /XD ".svn"
+
+if errorLevel 8 (
+    echo.
+    echo *** ERROR: Copying files not sucessful
+    echo.
+	pause
+	exit
+)
+
+%DOTNETPATH%\Regasm "%LOCALDIR%\IESpeedLibrary.dll" /tlb /codebase
+
+regedit /s "%STAGEDIR%\iespeed_mark_as_safe.reg"
+
+if errorLevel 1 (
+    echo.
+    echo *** ERROR: Importing registry entries not successful
+    echo.
+	pause
+	exit
+)
+
+echo.
+echo -------------------------------------------------------
+echo SUCESSFULLY installed IESpeed
+echo -------------------------------------------------------
+echo.
+
+pause
+
